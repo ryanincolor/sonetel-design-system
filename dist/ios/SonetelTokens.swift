d0adad416b9bc542864bdfade5bb869900fd1fbd
ryanpittman <ryan.pittman@bambuser.com>
2025-07-17 15:26:19 +0200

    add new files

--- dist/ios/SonetelTokens.swift
+++ dist/ios/SonetelTokens.swift
@@ -1,24 +1,99 @@
-/**
- * SonetelTokens.swift
- * Design Tokens for iOS
- */
+
+//
+// SonetelTokens.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
 
 import UIKit
 
 public class SonetelTokens {
-    public static let spacing2xs: CGFloat = 2.0
-    public static let spacing4: CGFloat = 4.0
-    public static let spacing8: CGFloat = 8.0
-    public static let spacing16: CGFloat = 16.0
-    public static let spacing24: CGFloat = 24.0
-    public static let spacing32: CGFloat = 32.0
-    
-    public static let neutralSolidLight0 = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    public static let neutralSolidLight5 = UIColor(red: 0.96, green: 0.96, blue: 0.96, alpha: 1.0)
-    public static let neutralSolidDark0 = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
-    public static let neutralSolidDark5 = UIColor(red: 0.04, green: 0.04, blue: 0.04, alpha: 1.0)
-    
-    public static let huesBlue = UIColor(red: 0.39, green: 0.56, blue: 1.0, alpha: 1.0)
-    public static let huesRed = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
-    public static let huesGreen = UIColor(red: 0.48, green: 0.82, blue: 0.52, alpha: 1.0)
+    public static let 0 = #fff
+    public static let 0 = #000000
+    public static let 0 = #00000000
+    public static let 0 = #fff
+    public static let 0 = #00000000
+    public static let 1 = #f5f5f5
+    public static let 1 = #0000000a
+    public static let 10 = #f0f0f0
+    public static let 10 = #141414
+    public static let 10 = #0000000f
+    public static let 10 = #ffffff14
+    public static let 100 = #ffffff
+    public static let 100 = #000000
+    public static let 100 = #ffffff
+    public static let 12 = 12px
+    public static let 128 = 128px
+    public static let 16 = 16px
+    public static let 2 = 2px
+    public static let 2 = #f0f0f0
+    public static let 2 = #0000000f
+    public static let 20 = #e0e0e0
+    public static let 20 = #1f1f1f
+    public static let 20 = #0000001f
+    public static let 20 = #ffffff1f
+    public static let 20 = 20px
+    public static let 24 = 24px
+    public static let 28 = 28px
+    public static let 2xs = 2px
+    public static let 3 = #e0e0e0
+    public static let 3 = #0000001f
+    public static let 30 = #b8b8b8
+    public static let 30 = #292929
+    public static let 30 = #00000047
+    public static let 30 = #ffffff29
+    public static let 32 = 32px
+    public static let 4 = 4px
+    public static let 4 = #b8b8b8
+    public static let 4 = #00000047
+    public static let 40 = #8f8f8f
+    public static let 40 = #333333
+    public static let 40 = #00000070
+    public static let 40 = #ffffff33
+    public static let 48 = 48px
+    public static let 5 = #f5f5f5
+    public static let 5 = #0a0a0a
+    public static let 5 = #0000000a
+    public static let 5 = #ffffff0a
+    public static let 5 = #666666
+    public static let 5 = #00000099
+    public static let 50 = #666666
+    public static let 50 = #3d3d3d
+    public static let 50 = #00000099
+    public static let 50 = #f0f0f040
+    public static let 50 = 40px
+    public static let 56 = 56px
+    public static let 6 = #292929
+    public static let 6 = #000000d6
+    public static let 60 = #292929
+    public static let 60 = #808080
+    public static let 60 = #000000d6
+    public static let 60 = #ffffff80
+    public static let 64 = 64px
+    public static let 7 = #0a0a0a
+    public static let 7 = #000000
+    public static let 70 = #0a0a0a
+    public static let 70 = #b2b2b2
+    public static let 70 = #000000f5
+    public static let 70 = #ffffffb3
+    public static let 72 = 72px
+    public static let 8 = 8px
+    public static let 80 = 80px
+    public static let 96 = 96px
+    public static let blue = #638eff
+    public static let critical = #ff0000
+    public static let green = #7ad085
+    public static let inverse = #fff
+    public static let on-dark = #fff
+    public static let on-light = #0a0a0a
+    public static let orange = #ff9e9e
+    public static let pink = #ff68b0
+    public static let primary = #0a0a0a
+    public static let purple = #aaa1f1
+    public static let red = #ff0000
+    public static let seconday = #666666
+    public static let tertiary = #e0e0e0
+    public static let xs = 4px
+    public static let yellow = #ffef62
 }
